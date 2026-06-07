c4b7f8e0ca8b22bc8f7f24fccde7407e6dac9d08
Makshutsuliak <159724522+Makshutsuliak@users.noreply.github.com>
2026-06-07 09:32:17 +0300

    Update contactform.jsx

--- src/components/contactform/contactform.jsx
+++ src/components/contactform/contactform.jsx
@@ -76,7 +76,7 @@ const Contactform = () => {
     }
 
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("${VITE_API_URL}/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
