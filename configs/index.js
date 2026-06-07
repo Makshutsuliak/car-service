75ab4cd7258fb51daa382018431d67e9e5c761c4
Makshutsuliak <159724522+Makshutsuliak@users.noreply.github.com>
2026-06-07 13:12:12 +0300

    Update index.js

--- configs/index.js
+++ configs/index.js
@@ -3,6 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema.js";
 dotenv.config();
+console.log("DB type:", typeof db, db);
 
 if (!process.env.DRIZZLE_DATABASE_URL) {
   throw new Error("DRIZZLE_DATABASE_URL is not defined");
