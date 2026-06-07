c9369db430b29d4de9c1907000aa5d633a81e9f0
Makshutsuliak <159724522+Makshutsuliak@users.noreply.github.com>
2026-06-07 13:00:25 +0300

    Update [id].js

--- api/user/[id].js
+++ api/user/[id].js
@@ -1,15 +1,19 @@
 import { db } from "../../configs/index.js";
 import { Users } from "../../configs/schema.js";
-
 import { eq } from "drizzle-orm";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
+  const { method, query } = req;
+  const { id } = query;
+
+  if (method === "GET") {
     try {
-      const response = await db.select().from(Users).where(eq(Users.id, req.query.id));
-      res.status(200).json(response[0] || null);
-    } catch {
-      res.status(500).json({ error: "Помилка отримання користувача" });
+      const response = await db.select().from(Users).where(eq(Users.id, id));
+      return res.status(200).json(response[0] || null);
+    } catch (err) {
+      return res.status(500).json({ error: "Помилка отримання користувача" });
     }
   }
+
+  return res.status(405).json({ error: "Method not allowed" });
 }
