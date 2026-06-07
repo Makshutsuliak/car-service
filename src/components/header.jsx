5e9fed04b71d4e8e389077318f12f33da1eaf61c
Makshutsuliak <159724522+Makshutsuliak@users.noreply.github.com>
2026-06-07 16:26:33 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -56,27 +56,25 @@ const Header = () => {
   };
 
   return (
-    <div className='flex justify-between items-center shadow-sm p-5 m-5 mb-[100px] lg:mb-[0]'
-      onMouseLeave={() => setIsMenuOpen(false)}>
+    <div className='flex justify-between items-center shadow-sm p-5 m-5 mb-[100px] lg:mb-[0]'>
       <ScrollToTop />
 
       {/* меню */}
-      <div className="dropdown relative flex justify-around "
-        onMouseEnter={() => setIsMenuOpen(true)}>
-
+      <div className="dropdown relative flex justify-around">
         <img
-          className="mx-5"
+          className="mx-5 cursor-pointer"
           src="./logo.svg"
           alt="Логотип"
           width={150}
           height={300}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
         />
 
         <ul
           className={`absolute lg:static top-8 left-0 w-full sm:w-auto sm:bg-white shadow-md md:shadow-none lg:flex gap-16 text-center transition-all duration-300 ${isMenuOpen ? "block" : "hidden lg:flex"}`}
         >
           <Link to={"/"}>
-            <li className="bg-white  dropdown-item text-primary font-medium hover:scale-105 transition-all cursor-pointer p-3 md:p-0">
+            <li className="bg-white dropdown-item text-primary font-medium hover:scale-105 transition-all cursor-pointer p-3 md:p-0">
               Головна
             </li>
           </Link>
@@ -88,7 +86,8 @@ const Header = () => {
           <Link to={"/contactform"}>
             <li className="bg-white text-primary dropdown-item font-medium hover:scale-105 transition-all cursor-pointer bg-color-primary p-3 md:p-0">
               Записатись на обслуговування
-            </li></Link>
+            </li>
+          </Link>
           <Link to={"/questions"}>
             <li className="bg-white text-primary dropdown-item font-medium hover:scale-105 transition-all cursor-pointer bg-color-primary p-3 md:p-0">
               Відповіді на часті питання
