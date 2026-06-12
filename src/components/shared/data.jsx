5babfaf5cbbc4369f5185f618e8626a39ddcc82d
Makshutsuliak <159724522+Makshutsuliak@users.noreply.github.com>
2026-06-12 19:06:30 +0300

    Update data.jsx

--- src/components/shared/data.jsx
+++ src/components/shared/data.jsx
@@ -141,7 +141,7 @@ const Category =[
         { "id": 4, "description": "Огляд стану шин і колісних дисків" }
        
       ],
-      "image": "./category_1.png"
+      "image": "./public/category_1.png"
     },
     {
       "id": 2,
@@ -151,7 +151,7 @@ const Category =[
         { "id": 2, "description": "Заміна ременів, ланцюгів ГРМ, помпи" },        
         { "id": 3, "description": "Усунення стуків та вібрацій у підвісці" }
       ],
-      "image": "./category_2.png"
+      "image": "./public/category_2.png"
     },
     {
       "id": 3,
@@ -162,7 +162,7 @@ const Category =[
         { "id": 4, "description": "Полірування фар і кузова, відновлення лакофарбового покриття" }
       ],
         
-      "image": "./category_3.png"
+      "image": "./public/category_3.png"
     },
     {
       "id": 4,
@@ -173,7 +173,7 @@ const Category =[
         { "id": 4, "description": "Відновлення подушок безпеки (SRS) та систем пасивної безпеки"}
         
       ],
-      "image": "./category_4.png"
+      "image": "./public/category_4.png"
     }
   ]
   const faqData = [
